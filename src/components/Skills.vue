15bec5a9e8f273c95ad23c3ef0f79fd84f61e2f6
MortadhaFadhlaoui <mortadha.fadhlaoui@esprit.tn>
2018-08-09 12:34:31 +0200

    modif

---
+++ src/components/Skills.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">
+          {{index}}. {{data.skill}}
+        </li>
+        <p v-if="skills.length > 1"> You have more then one skills</p>
+        <p v-else> You have less than or equal to 1 skill</p>
+      </ul>
+    </div>
+    <!--{{ name }}
+    <br/>
+    {{btnState ? 'Disabled' : 'Enabled'}}
+    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+    data(){
+      return{
+         // name: 'Mortadha',
+         // btnState: true,
+          skills:[
+              {
+                  "skill": "Android"
+              },
+              {
+                  "skill": "IOS"
+              }
+          ]
+      }
+    },
+    methods: {
+      /*changeName: function () {
+          console.log("Changed");
+      }*/
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
